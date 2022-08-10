f2e1c8da12b71f5081cc3da2420a969343993690
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2022-08-10 22:48:17 +0200

    Add base class for checks

---
+++ app/checks/__init__.py
@@ -0,0 +1,15 @@
+ALL = []
+
+
+class CheckMeta(type):
+    def __init__(cls, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if object in cls.__bases__:
+            # Don't register base class
+            return
+        ALL.append(cls)
+
+
+class Check(metaclass=CheckMeta):
+    warnings = []
+    errors = []
