fed705ccd2a98de218bc07bfc99ec0bcd3319e48
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2022-08-12 14:20:31 +0200

    Add app-id check

---
+++ flatpak_builder_lint/checks/appid.py
@@ -0,0 +1,21 @@
+from . import Check
+
+
+class AppIDCheck(Check):
+    type = "manifest"
+
+    def check(self, manifest):
+        appid = manifest.get("id")
+        if not appid:
+            self.errors.append("appid-not-defined")
+            return
+
+        split = appid.split(".")
+        if len(split) < 3:
+            self.errors.append("appid-too-few-components")
+
+        if split[-1] == "desktop":
+            self.errors.append("appid-ends-with-lowercase-desktop")
+
+        if split[1].lower() in ("github", "gitlab") and split[0].lower() != "io":
+            self.warnings.append("appid-uses-code-hosting-domain")
