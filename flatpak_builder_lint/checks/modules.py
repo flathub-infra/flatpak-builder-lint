b253e61e102e03ccde160cc3d5d0ea93d3991e9b
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2022-09-10 11:16:57 +0200

    modules: Merge source checks

--- flatpak_builder_lint/checks/modules.py
+++ flatpak_builder_lint/checks/modules.py
@@ -1,19 +1,25 @@
-
-from .. import checks
 from . import Check
 
+
 class ModuleCheck(Check):
-    type = "module"
+    type = "manifest"
 
     def check(self, module):
-
         name = module.get("name")
-        buildsystem = module.get("buildsystem")
+        buildsystem = module.get("buildsystem", "autotools")
+
+        if buildsystem == "autotools":
+            config_opts = module.get("config-opts")
+            for opt in config_opts:
+                if opt.startswith("--prefix="):
+                    self.errors.append("module-autotools-redundant-prefix")
+                elif opt.startswith("--enable-debug"):
+                    self.errors.append("module-autotools-non-release-build")
 
         if buildsystem == "cmake":
             self.warnings.append("module-buildsystem-is-cmake")
 
-        if buildsystem == "cmake-ninja" or buildsystem == "cmake":
+        if buildsystem in ("cmake-ninja", "cmake"):
             config_opts = module.get("config-opts")
             for opt in config_opts:
                 if opt.startswith("-DCMAKE_INSTALL_PREFIX"):
@@ -24,29 +30,39 @@ class ModuleCheck(Check):
                         # we shouldn't emit this on an extension
                         self.warnings.append("module-cmake-no-debuginfo")
                     elif split[1] != "RelWithDebInfo":
-                        self.error.apend("module-cmake-non-release-build")
+                        self.errors.append("module-cmake-non-release-build")
 
-        elif buildsystem == "meson":
-            pass
-        elif buildsystem == "qmake":
-            pass
-        elif buildsystem == "simple":
-            pass
-        else:
-            config_opts = module.get("config-opts")
-            for opt in config_opts:
-                if opt.startswith("--prefix="):
-                    self.errors.append("module-autotools-redundant-prefix")
-                elif opt.startswith("--enable-debug"):
-                    self.errors.append("module-autotools-non-release-build")
-
-        sources = module.get("sources")
-        if not sources:
-            self.errors.append("module-no-sources")
-        else:
+        if sources := module.get("sources"):
             for source in sources:
-                for checkclass in checks.ALL:
-                    check = checkclass()
+                source_type = source.get("type")
+                dest_filename = source.get("dest-filename")
 
-                    if check.type == "source":
-                        check.check(source)
+                if dest_filename and dest_filename.find("/") != -1:
+                    self.errors.append("source-dest-filename-is-path")
+
+                if source_type == "archive" or source_type == "file":
+                    if source.get("sha1"):
+                        self.errors.append("source-sha1-deprecated")
+                    if source.get("md5"):
+                        self.errors.append("source-md5-deprecated")
+
+                if source_type == "git":
+                    if source.get("branch"):
+                        self.errors.append("source-git-branch")
+                    if not source.get("tag") and not source.get("commit"):
+                        self.errors.append("source-git-no-commit-or-tag")
+                    if source.get("path"):
+                        self.errors.append("source-git-path")
+                    url = source.get("url")
+                    if not url:
+                        self.errors.append("source-git-no-url")
+                    elif not url.startswith("https:") and not url.startswith("http:"):
+                        self.errors.append("source-git-url-not-http")
+
+                        for checkclass in checks.ALL:
+                            check = checkclass()
+
+                            if check.type == "source":
+                                check.check(source)
+        else:
+            self.errors.append("module-no-sources")
