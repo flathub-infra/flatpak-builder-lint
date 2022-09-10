ee45d679fdd03d8c2da16d4ba9021a5e0ade6371
Hubert Figuière <hub@figuiere.net>
2022-09-09 21:26:29 -0400

    checks: Added toplevel

---
+++ flatpak_builder_lint/checks/toplevel.py
@@ -0,0 +1,24 @@
+
+from . import Check
+
+class TopLevelCheck(Check):
+    type = "manifest"
+
+    def check(self, manifest):
+        build_extension = manifest.get("build-extension")
+
+        if not build_extension:
+            command = manifest.get("command")
+            if not command:
+                self.errors.append("toplevel-no-command")
+            elif command.startswith("/"):
+                self.warnings.append("toplevel-command-is-path")
+
+        branch = manifest.get("branch")
+
+        if branch == "stable" or branch == "master":
+            self.errors.append("toplevel-unecessary-branch")
+
+        default_branch = manifest.get("default-branch")
+        if default_branch == "stable" or default_branch == "master":
+            self.errors.append("toplevel-unecessary-default-branch")
