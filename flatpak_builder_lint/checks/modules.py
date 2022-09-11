5cbd23857569af4e34babbb9463df4fc68da2312
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2022-09-11 11:55:34 +0200

    modules: Add module name to messages and support for nested modules

--- flatpak_builder_lint/checks/modules.py
+++ flatpak_builder_lint/checks/modules.py
@@ -4,65 +4,73 @@ from . import Check
 class ModuleCheck(Check):
     type = "manifest"
 
-    def check(self, module):
+    def check_source(self, module_name, source):
+        source_type = source.get("type")
+        dest_filename = source.get("dest-filename")
+
+        if dest_filename and dest_filename.find("/") != -1:
+            self.errors.append(f"module-{module_name}-source-dest-filename-is-path")
+
+        if source_type == "archive" or source_type == "file":
+            if source.get("sha1"):
+                self.errors.append(f"module-{module_name}-source-sha1-deprecated")
+            if source.get("md5"):
+                self.errors.append(f"module-{module_name}-source-md5-deprecated")
+
+        if source_type == "git":
+            if source.get("branch"):
+                self.errors.append(f"module-{module_name}-source-git-branch")
+            if not source.get("tag") and not source.get("commit"):
+                self.errors.append(f"module-{module_name}-source-git-no-commit-or-tag")
+            if source.get("path"):
+                self.errors.append(f"module-{module_name}-source-git-local-path")
+            url = source.get("url")
+            if not url:
+                self.errors.append(f"module-{module_name}-source-git-no-url")
+            elif not url.startswith("https:") and not url.startswith("http:"):
+                self.errors.append(f"module-{module_name}-source-git-url-not-http")
+
+    def check_module(self, module):
         name = module.get("name")
         buildsystem = module.get("buildsystem", "autotools")
 
         if buildsystem == "autotools":
-            config_opts = module.get("config-opts")
-            for opt in config_opts:
-                if opt.startswith("--prefix="):
-                    self.errors.append("module-autotools-redundant-prefix")
-                elif opt.startswith("--enable-debug"):
-                    self.errors.append("module-autotools-non-release-build")
+            if config_opts := module.get("config-opts"):
+                for opt in config_opts:
+                    if opt.startswith("--prefix="):
+                        self.errors.append(f"module-{name}-autotools-redundant-prefix")
+                    elif opt.startswith("--enable-debug"):
+                        self.errors.append(f"module-{name}-autotools-non-release-build")
 
         if buildsystem == "cmake":
             self.warnings.append("module-buildsystem-is-cmake")
 
         if buildsystem in ("cmake-ninja", "cmake"):
-            config_opts = module.get("config-opts")
-            for opt in config_opts:
-                if opt.startswith("-DCMAKE_INSTALL_PREFIX"):
-                    self.errors.append("module-cmake-redundant-prefix")
-                elif opt.startswith("-DCMAKE_BUILD_TYPE"):
-                    split = opt.split("=")
-                    if split[1] == "Release":
-                        # we shouldn't emit this on an extension
-                        self.warnings.append("module-cmake-no-debuginfo")
-                    elif split[1] != "RelWithDebInfo":
-                        self.errors.append("module-cmake-non-release-build")
+            if config_opts := module.get("config-opts"):
+                for opt in config_opts:
+                    if opt.startswith("-DCMAKE_INSTALL_PREFIX"):
+                        self.errors.append(f"module-{name}-cmake-redundant-prefix")
+                    elif opt.startswith("-DCMAKE_BUILD_TYPE"):
+                        split = opt.split("=")
+                        if split[1] == "Release":
+                            # we shouldn't emit this on an extension
+                            self.warnings.append(f"module-{name}-cmake-no-debuginfo")
+                        elif split[1] != "RelWithDebInfo":
+                            self.errors.append(f"module-{name}-cmake-non-release-build")
 
         if sources := module.get("sources"):
             for source in sources:
-                source_type = source.get("type")
-                dest_filename = source.get("dest-filename")
-
-                if dest_filename and dest_filename.find("/") != -1:
-                    self.errors.append("source-dest-filename-is-path")
-
-                if source_type == "archive" or source_type == "file":
-                    if source.get("sha1"):
-                        self.errors.append("source-sha1-deprecated")
-                    if source.get("md5"):
-                        self.errors.append("source-md5-deprecated")
-
-                if source_type == "git":
-                    if source.get("branch"):
-                        self.errors.append("source-git-branch")
-                    if not source.get("tag") and not source.get("commit"):
-                        self.errors.append("source-git-no-commit-or-tag")
-                    if source.get("path"):
-                        self.errors.append("source-git-path")
-                    url = source.get("url")
-                    if not url:
-                        self.errors.append("source-git-no-url")
-                    elif not url.startswith("https:") and not url.startswith("http:"):
-                        self.errors.append("source-git-url-not-http")
-
-                        for checkclass in checks.ALL:
-                            check = checkclass()
-
-                            if check.type == "source":
-                                check.check(source)
+                self.check_source(module.get("name"), source)
         else:
-            self.errors.append("module-no-sources")
+            self.errors.append(f"module-{name}-no-sources")
+
+        if nested_modules := module.get("modules"):
+            for nested_module in nested_modules:
+                self.check_module(nested_module)
+
+    def check(self, manifest):
+        if modules := manifest.get("modules"):
+            for module in modules:
+                self.check_module(module)
+        else:
+            self.errors.append("toplevel-no-modules")
