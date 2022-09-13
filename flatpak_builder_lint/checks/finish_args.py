ae745bbc305f617c5fdd0bca10f6047c241d4369
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2022-09-13 09:38:31 +0200

    finish_args: Add typing

--- flatpak_builder_lint/checks/finish_args.py
+++ flatpak_builder_lint/checks/finish_args.py
@@ -6,7 +6,7 @@ from . import Check
 class FinishArgsCheck(Check):
     type = "manifest"
 
-    def check(self, manifest):
+    def check(self, manifest: dict) -> None:
         appid = manifest.get("id")
         finish_args_list = manifest.get("finish-args")
         build_extension = manifest.get("build-extension")
@@ -19,11 +19,12 @@ class FinishArgsCheck(Check):
             return
 
         fa = defaultdict(set)
-        for arg in finish_args_list:
-            split = arg.split("=")
-            key = split[0].removeprefix("--")
-            value = "=".join(split[1:])
-            fa[key].add(value)
+        if finish_args_list:
+            for arg in finish_args_list:
+                split = arg.split("=")
+                key = split[0].removeprefix("--")
+                value = "=".join(split[1:])
+                fa[key].add(value)
 
         if "x11" in fa["socket"] and "wayland" in fa["socket"]:
             self.errors.append("finish-args-contains-both-x11-and-wayland")
@@ -46,8 +47,10 @@ class FinishArgsCheck(Check):
         for own_name in fa["own-name"]:
             if own_name.startswith("org.kde.StatusNotifierItem"):
                 self.errors.append("finish-args-broken-kde-tray-permission")
-            elif own_name.startswith(appid):
-                self.errors.append("finish-args-unnecessary-appid-own-name")
+
+            if appid:
+                if own_name.startswith(appid):
+                    self.errors.append("finish-args-unnecessary-appid-own-name")
 
         if (
             "xdg-config/autostart" in fa["filesystem"]
