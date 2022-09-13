9b6d855e0ffdda6584c79433fe48843c35f1c3e1
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2022-09-13 09:36:27 +0200

    checkmeta: Ignore mypy complainings about args and kwargs

--- flatpak_builder_lint/checks/__init__.py
+++ flatpak_builder_lint/checks/__init__.py
@@ -4,7 +4,7 @@ ALL = []
 
 
 class CheckMeta(type):
-    def __init__(cls, *args, **kwargs):
+    def __init__(cls, *args, **kwargs) -> None:  # type: ignore
         super().__init__(*args, **kwargs)
         if object in cls.__bases__:
             # Don't register base class
