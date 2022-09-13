45298168917b66a96444d8989a239cfb158611fb
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2022-09-13 09:39:11 +0200

    tools: Silence mypy complaining about json module

--- flatpak_builder_lint/tools.py
+++ flatpak_builder_lint/tools.py
@@ -2,12 +2,11 @@ import errno
 import json
 import os
 import subprocess
-import typing
 
 
 # json-glib supports non-standard syntax like // comments. Bail out and
 # delegate parsing to flatpak-builder.
-def show_manifest(filename: str) -> typing.Dict:
+def show_manifest(filename: str) -> dict:
     if not os.path.exists(filename):
         raise OSError(errno.ENOENT)
 
@@ -21,4 +20,7 @@ def show_manifest(filename: str) -> typing.Dict:
     manifest = ret.stdout.decode("utf-8")
     manifest_json = json.loads(manifest)
     manifest_json["x-manifest-filename"] = filename
-    return manifest_json
+
+    # mypy does not support circular types
+    # https://github.com/python/typing/issues/182
+    return manifest_json  # type: ignore
