26a2e6ea66b0033676f6238aacf5ce07a08b7a2b
Hubert Figuière <hub@figuiere.net>
2022-09-17 13:06:55 -0400

    appid: support codeberg

--- flatpak_builder_lint/checks/appid.py
+++ flatpak_builder_lint/checks/appid.py
@@ -23,8 +23,13 @@ class AppIDCheck(Check):
         if split[-1] == "desktop":
             self.errors.append("appid-ends-with-lowercase-desktop")
 
-        if split[1].lower() in ("github", "gitlab"):
-            if split[0].lower() != "io":
+        domain = split[1].lower()
+        tld = split[0].lower()
+        if domain in ("github", "gitlab", "codeberg"):
+            if tld != "io" and domain in ("github", "gitlab"):
+                self.warnings.append("appid-uses-code-hosting-domain")
+            # Codeberg: https://codeberg.page/
+            if tld != "page" and domain == "codeberg":
                 self.warnings.append("appid-uses-code-hosting-domain")
             if len(split) < 4:
                 self.errors.append("appid-code-hosting-too-few-components")
