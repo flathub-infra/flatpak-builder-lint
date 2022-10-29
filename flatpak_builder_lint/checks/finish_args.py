803f68265cf0e2f0d149d9ed078af6e71e4b6548
Hubert Figuière <hub@figuiere.net>
2022-10-29 12:04:00 -0400

    Issue #54 - redundant device-all is a warning
    - It doesn't weaken the sandbox

--- flatpak_builder_lint/checks/finish_args.py
+++ flatpak_builder_lint/checks/finish_args.py
@@ -81,7 +81,7 @@ class FinishArgsCheck(Check):
             self.warnings.add("finish-args-deprecated-shm")
 
         if "all" in fa["device"] and len(fa["device"]) > 1:
-            self.errors.add("finish-args-redundant-device-all")
+            self.warnings.add("finish-args-redundant-device-all")
 
         if "org.freedesktop.Flatpak" in fa["talk-name"]:
             self.errors.add("finish-args-flatpak-spawn-access")
