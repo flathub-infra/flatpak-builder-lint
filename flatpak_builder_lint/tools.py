e8328f5635dfd9f09416f6af1b9302eb26a96f92
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2023-09-25 19:54:50 +0200

    tools: Add function for parsing Flatpak metadata

--- flatpak_builder_lint/tools.py
+++ flatpak_builder_lint/tools.py
@@ -2,7 +2,8 @@ import errno
 import json
 import os
 import subprocess
-
+from collections import defaultdict
+from configparser import ConfigParser
 from typing import Optional
 
 
@@ -14,7 +15,7 @@ def show_manifest(filename: str) -> dict:
         raise OSError(errno.ENOENT)
 
     ret = subprocess.run(
-        ["flatpak-builder", "--show-manifest", filename], capture_output=True
+        ["flatpak-builder", "--show-manifest", filename], capture_output=True,
     )
 
     if ret.returncode != 0:
@@ -27,7 +28,7 @@ def show_manifest(filename: str) -> dict:
     manifest_basedir = os.path.dirname(filename)
     flathub_json_path = os.path.join(manifest_basedir, "flathub.json")
     if os.path.exists(flathub_json_path):
-        with open(flathub_json_path, "r") as f:
+        with open(flathub_json_path) as f:
             flathub_json = json.load(f)
             manifest_json["x-flathub"] = flathub_json
 
@@ -39,3 +40,72 @@ def show_manifest(filename: str) -> dict:
 def infer_appid_from_manifest(filename: str) -> Optional[str]:
     manifest = show_manifest(filename)
     return manifest.get("id")
+
+
+def get_metadata(builddir: str):
+    if not os.path.exists(builddir):
+        raise OSError(errno.ENOENT)
+
+    metadata_path = os.path.join(builddir, "metadata")
+    if not os.path.exists(metadata_path):
+        raise OSError(errno.ENOENT)
+
+    parser = ConfigParser()
+    parser.read(metadata_path)
+
+    if "Application" not in parser:
+        return None
+
+    metadata = dict(parser["Application"])
+
+    if "tags" in metadata:
+        tags = [x for x in metadata["tags"].split(";") if x]
+        metadata["tags"] = tags
+
+    permissions = {}
+
+    if "Context" in parser:
+        for key in parser["Context"]:
+            permissions[key] = [x for x in parser["Context"][key].split(";") if x]
+        parser.remove_section("Context")
+
+    if "Session Bus Policy" in parser:
+        bus_metadata = parser["Session Bus Policy"]
+        bus = defaultdict(list)
+
+        for busname in bus_metadata:
+            bus_permission = bus_metadata[busname]
+            bus[bus_permission].append(busname)
+
+        permissions["session-bus"] = bus
+
+    if "System Bus Policy" in parser:
+        bus_metadata = parser["System Bus Policy"]
+        bus = defaultdict(list)
+
+        for busname in bus_metadata:
+            bus_permission = bus_metadata[busname]
+            bus[bus_permission].append(busname)
+
+        permissions["system-bus"] = bus
+
+    metadata["permissions"] = permissions
+
+    extensions = {}
+    for section in parser:
+        if section.startswith("Extension "):
+            extname = section[10:]
+            extensions[extname] = dict(parser[section])
+
+    if extensions:
+        metadata["extensions"] = extensions
+
+    if "Build" in parser:
+        metadata["built-extensions"] = [
+            x for x in parser.get("Build", "built-extensions").split(";") if x
+        ]
+
+    if "Extra Data" in parser:
+        metadata["extra-data"] = dict(parser["Extra Data"])
+
+    return metadata
