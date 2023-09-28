555fc7c00b8d472dfabcd021440ab47a13c7e48f
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2023-09-28 11:19:00 +0200

    Add metainfo checks

---
+++ flatpak_builder_lint/appstream.py
@@ -0,0 +1,18 @@
+import subprocess
+import os
+
+def validate(path: str) -> dict:
+    if not os.path.isfile(path):
+        raise FileNotFoundError("AppStream file not found")
+    cmd = subprocess.run(
+            ["appstream-util", "validate", path],
+            capture_output=True,
+            )
+
+    ret = {
+            "stdout": cmd.stdout.decode("utf-8"),
+            "stderr": cmd.stderr.decode("utf-8"),
+            "returncode": cmd.returncode,
+            }
+
+    return ret
