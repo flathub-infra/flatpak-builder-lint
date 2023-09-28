af55d00bfcd40f6cbe262e7f3d7f0aa24560b4ed
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2023-09-28 12:13:07 +0200

    Add helper library for running appstream-util validate

--- flatpak_builder_lint/appstream.py
+++ flatpak_builder_lint/appstream.py
@@ -1,18 +1,19 @@
-import subprocess
 import os
+import subprocess
+
 
 def validate(path: str) -> dict:
     if not os.path.isfile(path):
         raise FileNotFoundError("AppStream file not found")
     cmd = subprocess.run(
-            ["appstream-util", "validate", path],
-            capture_output=True,
-            )
+        ["appstream-util", "validate", path],
+        capture_output=True,
+    )
 
     ret = {
-            "stdout": cmd.stdout.decode("utf-8"),
-            "stderr": cmd.stderr.decode("utf-8"),
-            "returncode": cmd.returncode,
-            }
+        "stdout": cmd.stdout.decode("utf-8"),
+        "stderr": cmd.stderr.decode("utf-8"),
+        "returncode": cmd.returncode,
+    }
 
     return ret
