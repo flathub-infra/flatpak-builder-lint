39b9a73b6cee344c7a4ffe3a226c3e69e0ea271e
Razze <razzeee@gmail.com>
2023-10-03 15:41:13 +0200

    Check for missing developer name

--- flatpak_builder_lint/appstream.py
+++ flatpak_builder_lint/appstream.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 
+from lxml import etree  # type: ignore
+
 
 def validate(path: str) -> dict:
     if not os.path.isfile(path):
@@ -17,3 +19,15 @@ def validate(path: str) -> dict:
     }
 
     return ret
+
+
+def is_developer_name_present(path: str) -> bool:
+    if not os.path.isfile(path):
+        raise FileNotFoundError("AppStream file not found")
+
+    root = etree.parse(path)
+    components = root.xpath("/components/component")
+
+    developer = components[0].xpath("developer_name")
+
+    return bool(developer)
