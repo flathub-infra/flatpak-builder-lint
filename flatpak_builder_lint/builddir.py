flathub-infra/flatpak-builder-lint

commit b997423a39c77f9a173ba075b9b288a2d1f32f88

builddir: Remove x11 from sockets if fallback-x11 was also defined

flatpak_builder_lint/builddir.py
@@ -68,6 +68,9 @@ def parse_metadata(ini: str) -> dict:
     if "sockets" in permissions:
         permissions["socket"] = permissions.pop("sockets")
 
+    if "x11" in permissions["socket"] and "fallback-x11" in permissions["socket"]:
+        permissions["socket"].remove("x11")
+
     if "devices" in permissions:
         permissions["device"] = permissions.pop("devices")
 

pyproject.toml
@@ -1,6 +1,6 @@
 [tool.poetry]
 name = "flatpak_builder_lint"
-version = "2.0.6"
+version = "2.0.8"
 description = "A linter for flatpak-builder manifests"
 authors = ["Bartłomiej Piotrowski <b@bpiotrowski.pl>"]
 license = "MIT"

commit 15870699aab5f2e2ae5ae462b85c2e8862daad5d

Fix tests
They were broken by b997423a39c77f9a173ba075b9b288a2d1f32f88

- x11-fallback is removed so it no longer appear in the output
- if permission is none, checking the value for a key turn it to
a set, cause the test to fail. Reduce the side effect.

flatpak_builder_lint/builddir.py
@@ -68,7 +68,11 @@ def parse_metadata(ini: str) -> dict:
     if "sockets" in permissions:
         permissions["socket"] = permissions.pop("sockets")
 
-    if "x11" in permissions["socket"] and "fallback-x11" in permissions["socket"]:
+    if (
+        "sockets" in permissions
+        and "x11" in permissions["socket"]
+        and "fallback-x11" in permissions["socket"]
+    ):
         permissions["socket"].remove("x11")
 
     if "devices" in permissions:

tests/test_builddir.py
@@ -32,11 +32,9 @@ def test_builddir_finish_args() -> None:
     }
 
     warnings = {
-        "finish-args-contains-both-x11-and-wayland",
         "finish-args-deprecated-shm",
         "finish-args-x11-without-ipc",
         "finish-args-redundant-device-all",
-        "finish-args-contains-both-x11-and-fallback",
     }
 
     ret = run_checks("tests/builddir/finish_args")