ed5e2280e7a2b46f6667854254d47325126a952a
Hubert Figuière <hub@figuiere.net>
2023-11-14 08:55:56 -0500

    Revert "Better check for xdg-dir access"
    - this trigger in unexpected conditions and needs to be reworked
    
    This reverts commit 7869b06fd6b70f6cf8d63860274aff9f8925c155.
    
    Signed-off-by: Hubert Figuière <hub@figuiere.net>

--- flatpak_builder_lint/checks/finish_args.py
+++ flatpak_builder_lint/checks/finish_args.py
@@ -1,4 +1,3 @@
-import re
 from collections import defaultdict
 from typing import Optional, Set
 
@@ -25,12 +24,11 @@ class FinishArgsCheck(Check):
             self.errors.add("finish-args-fallback-x11-without-wayland")
 
         for xdg_dir in ["xdg-data", "xdg-config", "xdg-cache"]:
-            regexp_arbitrary = f"^{xdg_dir}(:(create|rw|ro)?)?$"
-            regexp_unnecessary = f"^{xdg_dir}(\\/.*)?(:(create|rw|ro)?)?$"
+            if xdg_dir in finish_args["filesystem"]:
+                self.errors.add(f"finish-args-arbitrary-{xdg_dir}-access")
+
             for fs in finish_args["filesystem"]:
-                if re.match(regexp_arbitrary, fs):
-                    self.errors.add(f"finish-args-arbitrary-{xdg_dir}-access")
-                elif re.match(regexp_unnecessary, fs):
+                if fs.startswith(f"{xdg_dir}/") and fs.endswith(":create"):
                     self.errors.add(f"finish-args-unnecessary-{xdg_dir}-access")
 
         if "home" in finish_args["filesystem"] and "host" in finish_args["filesystem"]:
