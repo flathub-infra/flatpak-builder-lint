769a5b83ca4ede117c0647dd711f7960ccb372a8
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2024-02-14 11:29:56 +0100

    appstream: Support the new <developer> tag in is_developer_name_present

--- flatpak_builder_lint/appstream.py
+++ flatpak_builder_lint/appstream.py
@@ -61,8 +61,9 @@ def components(path: str) -> list:
 
 
 def is_developer_name_present(path: str) -> bool:
-    developer = components(path)[0].xpath("developer_name")
-    return bool(developer)
+    developer_name = components(path)[0].xpath("developer/name")
+    legacy_developer_name = components(path)[0].xpath("developer_name")
+    return bool(developer_name or legacy_developer_name)
 
 
 def is_project_license_present(path: str) -> bool:
