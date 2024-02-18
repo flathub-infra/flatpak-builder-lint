9c23d4d02b628dbb4648d68af9f3fba932c5bd5f
Dark Dragon <darkdragon-001@web.de>
2024-02-18 22:39:32 +0100

    Move build steps into container to actually build for other architectures

--- docker/build.sh
+++ docker/build.sh
@@ -6,12 +6,11 @@ git config --global protocol.file.allow always
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 [[ ! -d org.flatpak.Builder ]] && git clone https://github.com/flathub/org.flatpak.Builder
 
-sudo apt-get install -y python3-requirement-parser python3-toml
 mv flatpak-builder-lint-deps.json org.flatpak.Builder/flatpak-builder-lint-deps.json
 python3 rewrite-manifest.py
 
 cd org.flatpak.Builder 
 flatpak-builder --user --verbose --force-clean --repo=repo \
-    --ccache --install-deps-from=flathub \
+    --disable-rofiles-fuse --ccache --install-deps-from=flathub \
     builddir org.flatpak.Builder.json
 rm -rf files/lib/debug
