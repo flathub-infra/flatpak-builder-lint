97fd6ba29213d659e87ff7d36864f1955c971098
Bartłomiej Piotrowski <b@bpiotrowski.pl>
2024-02-19 10:17:02 +0100

    use matrix to build for each arch separately

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+sudo apt-get update
+sudo apt-get install -y flatpak-builder git bzip2 git \
+    python3 python3-requirement-parser python3-toml
+
 git config --global protocol.file.allow always
 
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
@@ -9,8 +13,16 @@ flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/fl
 mv flatpak-builder-lint-deps.json org.flatpak.Builder/flatpak-builder-lint-deps.json
 python3 rewrite-manifest.py
 
+case $1 in
+    linux/amd64)
+        arch=x86_64
+        ;;
+    linux/arm64)
+        arch=aarch64
+        ;;
+esac
+
 cd org.flatpak.Builder 
-flatpak-builder --user --verbose --force-clean --repo=repo \
-    --disable-rofiles-fuse --ccache --install-deps-from=flathub \
-    builddir org.flatpak.Builder.json
-rm -rf files/lib/debug
+flatpak-builder --arch=$arch --user --verbose --force-clean --repo=repo \
+    --ccache --install-deps-from=flathub builddir org.flatpak.Builder.json
+rm -rf "builddir/files/lib/debug"
