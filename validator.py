4de2ae83943553d787e5fe74826452cd6417f89e
bbhtt <bbhtt.zn0i8@slmail.me>
2024-02-20 17:12:57 +0530

    validator: Catch unknown exceptions sneaking into the list

--- validator.py
+++ validator.py
@@ -23,7 +23,19 @@ def main(argv: Sequence[str] | None = None) -> int:
     for filename in args.filenames:
         with open(filename, "r") as f:
             try:
-                json.load(f, object_pairs_hook=check_duplicates)
+                data = json.load(f, object_pairs_hook=check_duplicates)
+                found_exceptions = {
+                    j for i in data.values() for j in i if not j.startswith("module-")
+                } - {
+                    "*",
+                    "appid-filename-mismatch",
+                    "flathub-json-deprecated-i386-arch-included",
+                    "toplevel-no-command",
+                    "flathub-json-skip-appstream-check",
+                }
+                if not found_exceptions.issubset(known_exceptions):
+                    print("Exception not found in known exceptions list", found_exceptions - known_exceptions)
+                    exit_code = 1
             except ValueError as err:
                 print(f"{filename}: Failed to decode: {err}")
                 exit_code = 1
