c977d6f9b2ea1d5d39f4902c3d3331ac95790ec9
Hubert Figuière <hub@figuiere.net>
2026-01-21 19:09:24 -0500

    manifest: Check URL pointing to tip of branches for gitlab
    Signed-off-by: Hubert Figuière <hub@figuiere.net>

--- flatpak_builder_lint/giturl.py
+++ flatpak_builder_lint/giturl.py
@@ -1,5 +1,5 @@
 import re
-import urllib
+from urllib.parse import parse_qs, urlsplit
 
 
 def is_git_commit_hash(s: str) -> bool:
@@ -17,9 +17,33 @@ def _is_path_gh_branch(components: list[str]) -> bool:
     return components[0] in ("main", "master")
 
 
+def _is_gitlab_separator(component: str) -> bool:
+    return component == "-" or _is_gitlab_prefix(component)
+
+
+def _is_gitlab_prefix(component: str) -> bool:
+    return component in ("raw", "issues", "merge_requests", "tree", "jobs", "releases")
+
+
+def _is_path_gitlab_branch(components: list[str]) -> bool:
+    if components[0] == "-":
+        components.pop(0)
+
+    if len(components) < 2:
+        return False
+
+    if components[0] == "raw":
+        if is_git_commit_hash(components[1]):
+            return False
+        if components[1] in ("main", "master"):
+            return True
+
+    return False
+
+
 # Check if the URL to code hosting an a branch
 def is_branch(url: str) -> bool:
-    u = urllib.parse.urlsplit(url)
+    u = urlsplit(url)
     if u is None:
         return False
     hostname = u.hostname
@@ -34,7 +58,7 @@ def is_branch(url: str) -> bool:
         if raw in "blob":
             query = u.query
             if query:
-                q = urllib.parse.parse_qs(query)
+                q = parse_qs(query)
                 return q["raw"] == ["true"] and _is_path_gh_branch(path_components)
 
         return False
@@ -42,4 +66,21 @@ def is_branch(url: str) -> bool:
         path_components.pop(0)  # user / org
         path_components.pop(0)  # project / repo
         return _is_path_gh_branch(path_components)
+
+    if hostname in ("gitlab.com", "gitlab.gnome.org", "gitlab.freedesktop.org", "invent.kde.org"):
+        # API endpoints start with "api".
+        if path_components[0] in ("api"):
+            return False
+
+        path_components.pop(0)  # user / org
+        path_components.pop(0)  # project / repo
+        if len(path_components) < 1:
+            return False
+        # There can be a user/project/repo
+        if not _is_gitlab_separator(path_components[0]):
+            path_components.pop(0)  # Third component is not '-' or other prefixes
+
+        if len(path_components) > 0:
+            return _is_path_gitlab_branch(path_components)
+
     return False
--- tests/test_giturl.py
+++ tests/test_giturl.py
@@ -23,3 +23,43 @@ def test_is_branch() -> None:
     assert not giturl.is_branch(
         "https://github.com/user/project/blob/master/screenshots/Screenshot.png"
     )
+
+    ## Gitlab
+    # Basic image at a commit
+    assert not giturl.is_branch(
+        "https://gitlab.com/user/project/-/raw/ab5356ba2ce36e3ee0bab6e30cc051ad07c49ecd/img/logo.svg"
+    )
+    assert giturl.is_branch("https://gitlab.com/user/project/-/raw/main/screenshots/small.png")
+    assert giturl.is_branch(
+        "https://gitlab.com/user3/project3/-/raw/master/src/images/screenshot.png"
+    )
+    # With org/group/project URL
+    assert giturl.is_branch("https://gitlab.com/user4/group1/app2/-/raw/master/screenshot.png")
+    assert not giturl.is_branch(
+        "https://gitlab.com/user4/group1/app2/-/raw/30f79bec32243c31dd91a05c0ad7b80f1e301aea/screenshot.png"
+    )
+    # old syntax (no `-`)
+    assert not giturl.is_branch(
+        "https://gitlab.com/user2/project3/raw/1.0.0b1/data/image/org.flathub.Builder.svg"
+    )
+    assert not giturl.is_branch(
+        "https://gitlab.com/user2/project2/raw/30f79bec32243c31dd91a05c0ad7b80f1e301aea/data/image/org.flathub.Builder.svg"
+    )
+    assert giturl.is_branch(
+        "https://gitlab.com/user2/project2/raw/main/data/image/org.flathub.Builder.svg"
+    )
+    # This is a tree. Doesn't lead to a file.
+    assert not giturl.is_branch("https://gitlab.com/user4/project6/-/tree/master/po")
+    # Blob is the webpage.
+    assert not giturl.is_branch(
+        "https://gitlab.com/user/project3/-/blob/main/screenshots/pic-1.png"
+    )
+    # Other top-level: issues
+    assert not giturl.is_branch("https://gitlab.com/user3/project2/issues")
+    # Other top-level: CI artifact
+    assert not giturl.is_branch(
+        "https://gitlab.com/user6/core/-/jobs/7109755574/artifacts/raw/app2/tarball123.zip"
+    )
+    # Repositories
+    assert not giturl.is_branch("https://gitlab.com/user1/group2tools/app3.git")
+    assert not giturl.is_branch("https://gitlab.com/user2/project4.git")
