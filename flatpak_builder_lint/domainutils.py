0e0d190bed7fdf31e35e7a1e554b0197f0f58fa8
bbhtt <bbhtt.zn0i8@slmail.me>
2026-03-14 14:48:44 +0530

    Support per-repo remote exceptions
    If --exceptions-repo is not supplied, exceptions from all
    repo keys are merged (backwards compatible behaviour). [1]
    
    If supplied, exceptions from the given repo key are union'd with
    exceptions under the "*" repo key.
    
    The user exceptions option remains unchanged.
    
    Direct uploads and flat-manager-hooks will continue working same as
    before due to [1], but we will update flat-manager-hooks to supply
    the new arg later.
    
    Vorarbeiter will also be updated to use the new `--exceptions-repo`
    argument.
    
    In the next commit we migrate all of our existing exceptions under
    the `*` key so that they continue working.
    
    Any new exceptions accepted or modified needs to be under a per-repo
    key. Tooling will be added in a later commit to catch this.

--- flatpak_builder_lint/domainutils.py
+++ flatpak_builder_lint/domainutils.py
@@ -230,7 +230,7 @@ def check_url(url: str, strict: bool = False) -> tuple[bool, str | None]:
 
 
 @cache
-def get_remote_exceptions_flathub(appid: str) -> set[str]:
+def get_remote_exceptions_flathub(appid: str, exceptions_repo: str | None) -> set[str]:
     url = f"{config.FLATHUB_API_URL}/exceptions/{appid}"
     try:
         # exception updates should be reflected immediately
@@ -242,8 +242,21 @@ def get_remote_exceptions_flathub(appid: str) -> set[str]:
         )
         logger.debug("Response headers for %s: %s", url, dict(r.headers))
         if r.status_code == 200 and r.headers.get("Content-Type") == "application/json":
-            logger.debug("Loaded remote exceptions from %s: %s", url, set(r.json()))
-            return set(r.json())
+            app_exceptions = r.json().get(appid, {})
+            if exceptions_repo:
+                result = set(app_exceptions.get(exceptions_repo, {}).keys()) | set(
+                    app_exceptions.get("*", {}).keys()
+                )
+            else:
+                result = {k for v in app_exceptions.values() for k in v}
+            logger.debug(
+                "Loaded remote exceptions for %s (repo key: %s) from %s: %s",
+                appid,
+                exceptions_repo,
+                url,
+                result,
+            )
+            return result
     except requests.exceptions.RequestException as e:
         logger.debug(
             "Request exception when fetching exceptions for %s: %s: %s", appid, type(e).__name__, e
@@ -253,7 +266,7 @@ def get_remote_exceptions_flathub(appid: str) -> set[str]:
 
 
 @cache
-def get_remote_exceptions_github(appid: str) -> set[str]:
+def get_remote_exceptions_github(appid: str, exceptions_repo: str | None) -> set[str]:
     url = (
         f"{config.GITHUB_CONTENT_CDN}/{config.LINTER_FULL_REPO}/"
         f"HEAD/flatpak_builder_lint/staticfiles/exceptions.json"
@@ -266,13 +279,21 @@ def get_remote_exceptions_github(appid: str) -> set[str]:
         )
         logger.debug("Response headers for %s: %s", url, dict(r.headers))
         if r.status_code == 200 and r.headers.get("Content-Type", "").startswith("text/plain"):
+            app_exceptions = r.json().get(appid, {})
+            if exceptions_repo:
+                result = set(app_exceptions.get(exceptions_repo, {}).keys()) | set(
+                    app_exceptions.get("*", {}).keys()
+                )
+            else:
+                result = {k for v in app_exceptions.values() for k in v}
             logger.debug(
-                "Loaded remote exceptions for %s from %s: %s",
+                "Loaded remote exceptions for %s (repo key: %s) from %s: %s",
                 appid,
+                exceptions_repo,
                 url,
-                set(r.json().get(appid, {}).keys()),
+                result,
             )
-            return set(r.json().get(appid, {}).keys())
+            return result
     except requests.exceptions.RequestException as e:
         logger.debug(
             "Request exception when fetching exceptions for %s: %s: %s", appid, type(e).__name__, e
